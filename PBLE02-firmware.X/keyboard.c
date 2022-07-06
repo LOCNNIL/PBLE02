09491fba1357bda1954cdcc2167aaa874dc2398f
Anderson Guerra de Almeida <dersonguerra@gmail.com>
2022-07-05 23:52:11 -0300

    fix keyboard lib

--- PBLE02-firmware.X/keyboard.c
+++ PBLE02-firmware.X/keyboard.c
@@ -3,6 +3,16 @@
 #include "mcc_generated_files/adc1.h"
 #include "Utils.h"
 
+
+const uint16_t keys[6] = {
+    0, 918, 1511, 2208, 2940, 3691
+};
+
+uint32_t key_btns = 0;
+uint32_t key_cnt = 0;
+uint32_t key_lstChangeSW = 0;
+uint32_t keySum = 0;
+
 void updateButtons(uint32_t m){
     
     ADC1_Enable();
@@ -12,26 +22,30 @@ void updateButtons(uint32_t m){
     while(!ADC1_IsConversionComplete(SW));
     keySum += ADC1_ConversionResultGet(SW);
     ADC1_Disable();
-    cnt++;
-    if(cnt < (1 << NSAMPLES_2P_SW)) return;
+    key_cnt++;
+    if(key_cnt < (1 << NSAMPLES_2P_SW)) return;
     keySum = keySum >> NSAMPLES_2P_SW;
-    cnt = 0;
+    key_cnt = 0;
     
     for(uint8_t i = 0; i < 5; i++){
-        bitWrite(btns, i, keySum > keys[i] && keySum < keys[i + 1]);
-        if(bitRead(btns, i) != bitRead(btns, i + 5)){
-            bitWrite(btns, i + 5, bitRead(btns, i));
-            lstChangeSW = m;
-            bitClear(btns, 15);
+        bitWrite(key_btns, i, keySum > keys[i] && keySum < keys[i + 1]);
+        if(bitRead(key_btns, i) != bitRead(key_btns, i + 5)){
+            bitWrite(key_btns, i + 5, bitRead(key_btns, i));
+            key_lstChangeSW = m;
+            bitClear(key_btns, 15);
         }
     }
     
-    if(!bitRead(btns, 15)){
-        if(lstChangeSW - m > TIMECHANGE){
-            btns = (btns & 0x83FF) + ((btns & 0x03E0) << 5);
-            bitSet(btns,15);
+    if(!bitRead(key_btns, 15)){
+        if(key_lstChangeSW - m > TIMECHANGE){
+            key_btns = (key_btns & 0x83FF) + ((key_btns & 0x03E0) << 5);
+            bitSet(key_btns,15);
         }
     }
     
     keySum = 0;
+}
+
+uint8_t getButtons(){
+    return ((key_btns & 0x7C00) >> 10);
 }
--- PBLE02-firmware.X/keyboard.h
+++ PBLE02-firmware.X/keyboard.h
@@ -12,22 +12,12 @@
 extern "C" {
 #endif
 
-    
 #include "mcc_generated_files/system.h"
 
-    const uint16_t keys[6] = {
-        0, 918, 1511, 2208, 2940, 3691
-    };
-
-    uint32_t btns = 0;
-    uint32_t cnt = 0;
-    uint32_t lstChangeSW = 0;
-    uint32_t keySum = 0;
-    
 #define NSAMPLES_2P_SW 5 //MAX 20
 #define TIMECHANGE 5 //millisegundos
     
-    uint8_t getButtons(){ return ((btns & 0x7C00) >> 10); }
+    uint8_t getButtons();
     
     void updateButtons(uint32_t m);
     
